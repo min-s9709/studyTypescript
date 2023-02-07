b44a0dda348cac03bb0dae7bda5fa9e126108144
min-s9709 <78842816+min-s9709@users.noreply.github.com>
2023-02-07 20:03:09 +0900

    Typescript: Theme 적용해보기

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,17 @@
-import { useState } from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  background-color: ${(props) => props.theme.bgColor};
+`;
+const H1 = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
 
 const App = () => {
-  const [value, setValue] = useState("");
-  const onChange = (e: React.FormEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = e;
-    setValue(value);
-  };
-  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    console.log("hello", value);
-  };
   return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={value}
-          type="text"
-          placeholder="username"
-        />
-        <button>Log in</button>
-      </form>
-    </div>
+    <Container>
+      <H1>protected</H1>
+    </Container>
   );
 };
 
